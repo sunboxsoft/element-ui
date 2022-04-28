61cf2164af8ec912ee37128506a587cb3c4e76d1
sunboxsoft <sunboxsoft@163.com>
2022-04-28 17:51:04 +0800

    引用文件

--- src/main.js
+++ src/main.js
@@ -7,9 +7,15 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import vform from '../lib/vform/VFormDesigner.umd.min.js'
+import '../lib/vform/VFormDesigner.css'
+
+
+
 Vue.config.productionTip = false
 Vue.use(ElementUI);
-/* eslint-disable no-new */
+Vue.use(vform) //同时注册了v-form-designer、v-form-render等组件
+    /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
